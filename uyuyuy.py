374cd8409e34f53883653bdbdb569e0ec2b4613b
Guessdddd <linnnny200@gmail.com>
2025-05-23 17:14:44 +0500

    Add files via upload

--- uyuyuy.py
+++ uyuyuy.py
@@ -5,11 +5,11 @@ print('Привет')
 while True:
     ty = input("введи операцию(деление,умножения,сложение,вычетания)")
     if ty != 'стоп'
-        else: break
         a = int(input("первое число: "))
         b = int(input("второе число: "))
         if ty == 'деление':
             ptint(dele(a, b))
+            else: break
         elif ty == 'умножение':
             print(umnog(a, b))
         elif ty == 'сложение':
