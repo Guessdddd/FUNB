13c06b7aead5b46047cd11352b94ecbd3c454125
Guessdddd <linnnny200@gmail.com>
2025-05-23 17:01:16 +0500

    Add files via upload

---
+++ uyuyuy.py
@@ -0,0 +1,19 @@
+from FUNB import *
+from DYDYD import *
+print('Привет')
+
+while True:
+    ty = input("введи операцию(деление,умножения,сложение,вычетания)")
+    if ty == 'стоп'
+        a = int(input("первое число: "))
+        b = int(input("второе число: "))
+        if ty == 'деление':
+            ptint(dele(a, b))
+        elif ty == 'умножение':
+            print(umnog(a, b))
+        elif ty == 'сложение':
+            print(lonsh(a,b))
+        elif ty == 'вычетание':
+            print(vasut(a, b))
+
+
